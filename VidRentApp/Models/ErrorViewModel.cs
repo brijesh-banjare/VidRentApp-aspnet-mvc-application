4fa8f7937f71fd935009e6b90bb6190f0efb9b2d
bbanj <bbanj@DESKTOP-3RI3KJM>
2023-06-06 19:57:17 +0530

    Add project files.

---
+++ VidRentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VidRentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
